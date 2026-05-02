f0e488e76b8c960fabb7dc4bfdddd07e7a5a7476
Varun Nayak <vnayak1506@gmail.com>
2026-05-02 10:16:46 +0100

    Loaded the images in batches from looking at PyTorch docs, and have a basic plan in place for how to continue moving forward with what I'm doing

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
 from torchvision import datasets
+from torch.utils.data import DataLoader
 
 test_dataset = datasets.OxfordIIITPet(root = "./data", split = "test", target_types = "category",download = True)
+
+test_dataloader = DataLoader(test_dataset, batch_size = 32, shuffle = False)
+
+# Not particularly interesting in the order so I've set shuffle to false. If model accuracy drops, maybe change this to true and observe behaviour.
--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 from torchvision import datasets
+from torch.utils.data import DataLoader
 
 training_dataset = datasets.OxfordIIITPet(root = "./data", split = "trainval", target_types = "category",download = True)
 
@@ -11,3 +12,23 @@ print(first_image.size)
 
 image_label = training_dataset[0][1]
 print(image_label)
+
+training_dataloader = DataLoader(training_dataset, batch_size = 32, shuffle = True)
+
+
+# Batch size is the number of images that will be passed to the model at a time. Docs suggest using 64, but I've gone slightly lower to improve accuracy.
+# Use shuffle to avoid data bias by random shuffles at the start of each epoch to ensure that the model doesn't learn anything about the order of the images. 
+
+
+# Images are coming in different sizes so need to figure out how to resize them to a common size so the model can process them more easily. 
+# Need to be mindful of data loss when doing this though. 
+
+
+# Add loss function
+
+
+# Add optimiser
+
+# Coursework paper says to split the dataset into training and validation sets as an option. 
+# Number of images is greatly diluted already per the different classes, so I think I'll just keep the test set for validating. 
+# Could be interesting to see if it makes a difference if I do the 80:20 split for training and validation (with hyperparam modification to optimise) and then the test set for actually testing the model. 
