5e74fdcd6769c0fafc770ede7706971777ef91c8
Varun Nayak <vnayak1506@gmail.com>
2026-05-02 16:16:29 +0100

    Applied the image augmentation to the training data and minimal augmentation to the test data (resizing and tensor setup) using v2 of the transforms API and set up NN boilerplate, loss function, optimiser and studying PyTorch docs about how to implement my desired architecture

--- test.py
+++ test.py
@@ -1,10 +1,22 @@
 from torchvision import datasets
+from torchvision.transforms import v2
 from torch.utils.data import DataLoader
+import torch
 
-test_dataset = datasets.OxfordIIITPet(root = "./data", split = "test", target_types = "category",download = True)
+test_dataset = datasets.OxfordIIITPet(root = "./data", 
+        split = "test", 
+        target_types = "category",
+        download = True, 
+        transform = v2.Compose([
+            v2.Resize((224, 224)), 
+            v2.ToImage(),
+            v2.ToDtype(torch.float32, scale=True)]))
 
 test_dataloader = DataLoader(test_dataset, batch_size = 32, shuffle = False)
 
-# Not particularly interesting in the order so I've set shuffle to false. If model accuracy drops, maybe change this to true and observe behaviour.
+# Not particularly interesting in the order so I've set shuffle to false. 
+# If model accuracy drops, maybe change this to true and observe behaviour, but I don't think this will affect the model accuracy. 
+# It only affects the order in which the images are fed to the model. 
+
 
 # Add loss function
--- train.py
+++ train.py
@@ -1,17 +1,9 @@
-from torchvision import datasets, transforms
+from torchvision import datasets
+from torchvision.transforms import v2
 from torch.utils.data import DataLoader, random_split
 import matplotlib.pyplot as plt
-
-
-
-# Images are coming in different sizes so need to figure out how to resize them to a common size so the model can process them more easily. 
-# Need to be mindful of data loss when doing this though. 
-
-# Did some research and found that 224x224 is the conventional size for image classification tasks. If it takes too long to train, reduce the image size so it speeds up. 
-
-# From the week 10 lecture, the model expects data to be in the form of signals. 
-# toTensor() converts the pixel into a multi-deminsional array of numbers between 0 and 1 (kinda like a one-hot encoding). 
-# The model now has the numerical data it needs to learn from and now we can start training the model (no idea how to do this yet).
+import torch.nn as nn
+import torch
 
 training_losses = []
 validation_losses = []
@@ -21,31 +13,28 @@ training_dataset_full = datasets.OxfordIIITPet(root = "./data",
         split = "trainval", 
         target_types = "category",
         download = True, 
-        transform = transforms.Compose([
-            transforms.Resize((224, 224)), 
-            transforms.ToTensor()]))
+        transform = v2.Compose([
+            v2.Resize((224, 224)),
+            v2.RandomHorizontalFlip(),
+            v2.RandomRotation(10),
+            v2.ToImage(),
+            v2.ToDtype(torch.float32, scale=True)]))
 
 validation_dataset_full = datasets.OxfordIIITPet(root = "./data", 
         split = "trainval", 
         target_types = "category",
         download = True, 
-        transform = transforms.Compose([
-            transforms.Resize((224, 224)), 
-            transforms.ToTensor()]))
+        transform = v2.Compose([
+            v2.Resize((224, 224)), 
+            v2.ToImage(),
+            v2.ToDtype(torch.float32, scale=True)]))
 
 
-training_dataset, not_training = random_split(training_dataset_full, [int(0.8*len(training_dataset_full)), int(0.2*len(training_dataset_full))])
-not_validation, validation_dataset = random_split(validation_dataset_full, [int(0.8*len(validation_dataset_full)), int(0.2*len(validation_dataset_full))])
+training_images_num = int(0.8 * len(training_dataset_full))
+validation_images_num = int(0.2 * len(training_dataset_full))
 
-# print(len(training_dataset))
-
-# first_image = training_dataset[0][0]
-# print(first_image)
-
-# print(first_image.size)
-
-# image_label = training_dataset[0][1]
-# print(image_label)
+training_dataset, not_training = random_split(training_dataset_full, [training_images_num, validation_images_num])
+not_validation, validation_dataset = random_split(validation_dataset_full, [training_images_num, validation_images_num])
 
 # Set batch size to 32. If we wanna speed up training, increase to 64 per the documentation recommendations. 
 training_dataloader = DataLoader(training_dataset, batch_size = 32, shuffle = True)
@@ -53,16 +42,26 @@ validation_dataloader = DataLoader(validation_dataset, batch_size=32, shuffle = 
 
 print("Size of training dataset: "+str(len(training_dataset)))
 print("Size of validation dataset: "+str(len(validation_dataset)))
-# Need to find a way to increase the number of images available to us for training because we don't have enough currently. 
+
+class PetClassifier(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return x
 
 
+device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
+print(f"Using {device} device")
 
+
+pet_classifier = PetClassifier().to(device)
 # Add loss function
-
+nn_loss = nn.CrossEntropyLoss()
 
 # Add optimiser
-
-# Coursework paper says to split the dataset into training and validation sets as an option. 
+optimizer = torch.optim.Adam(pet_classifier.parameters(), lr=0.001)
+ 
 
 
 # Checking if we are overfitting or not
