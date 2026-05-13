7812222e245b8c54d12c992fccbc3771485a3dad
Varun Nayak <vnayak1506@gmail.com>
2026-05-13 12:25:38 +0100

    Managed to implement ResNet :) based off of all of the resources that I've found, studied and used for my benefit. I need to now figure out how I'm going to utilise this to my benefit and tailor things a bit more to my implementation. Found it super interesting

--- model.py
+++ model.py
@@ -2,6 +2,30 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch
 
+class ResBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        if stride != 1 or in_channels != out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels)
+            )
+        else:
+            self.shortcut = nn.Identity()
+
+    def forward(self, x):
+        identity = self.shortcut(x)
+        out = F.relu(self.bn1(self.conv1(x)), inplace=True)
+        out = self.bn2(self.conv2(out))
+        out = F.relu(out + identity, inplace=True)
+        return out
+
+
 class PetClassifier(nn.Module):
     def __init__(self):
         super().__init__()
